7549d64dee16cf4fbec89a9e2d1dfdba5765a7d3
lodubot <jaihacku@gmail.com>
2025-06-01 19:05:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+//base by DGXeon (Xeon Bot Inc.)
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: @DGXeon13
+//GitHub: @DGXeon13
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//telegram channel: https://t.me/xeonbotincorporated
+
+const global = {
+    owner: ["8100453801"],
+    botToken: "7958558348:AAE9bDTZYgGnt3XBoxqYCj_PaJFy_Ky3YYw",
+    GROUP_ID: "-1002597966668",
+    CHANNEL_ID: "-1002527035499",
+    GROUP_LINK: "https://t.me/+QTDvwwdYTpNhNjc1",
+    CHANNEL_INVITE_LINK: "https://t.me/xeonbotincorporated",
+    WHATSAPP_LINK: "https://whatsapp.com/channel/0029Vb5Z9SjIXnlvl97EQm2i",
+    YOUTUBE_LINK: "https://youtube.com/@dgxeon",
+    INSTAGRAM_LINK: "https://www.instagram.com/unicorn_xeon13"
+}
+
+module.exports = global;
